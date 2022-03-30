68d011d855272508a99b7c6271b445181c1e0e83
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-03-30 17:11:56 -0500

    adds Polygon network creds to config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,3 +38,30 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
+
+// Matic Network Credentials
+
+// require("@nomiclabs/hardhat-ethers");
+// const fs = require('fs');
+// const privateKey = fs.readFileSync(".secret").toString().trim();
+// module.exports = {
+//   defaultNetwork: "matic",
+//   networks: {
+//     hardhat: {
+//     },
+//     matic: {
+//       url: "https://rpc-mumbai.maticvigil.com",
+//       accounts: [process.env.PRIVATE_KEY]
+//     }
+//   },
+//   solidity: {
+//     version: "0.7.0",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   },
+// }
