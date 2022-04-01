51e971bc9d04200b02cdb68db83e0d5a1218efe3
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-04-01 03:09:21 -0500

    deployment adjustments

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,63 +5,62 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// // You need to export an object to set up your config
+// // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
-
+// /**
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+// module.exports = {
+//   solidity: "0.8.4",
+//   networks: {
+//     ropsten: {
+//       url: process.env.ROPSTEN_URL || "",
+//       accounts:
+//         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+//     },
+//   },
+//   gasReporter: {
+//     enabled: process.env.REPORT_GAS !== undefined,
+//     currency: "USD",
+//   },
+//   etherscan: {
+//     apiKey: process.env.ETHERSCAN_API_KEY,
+//   },
+// };
 
 // Matic Network Credentials
 
-// require("@nomiclabs/hardhat-ethers");
-// const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim();
-// module.exports = {
-//   defaultNetwork: "matic",
-//   networks: {
-//     hardhat: {
-//     },
-//     matic: {
-//       url: "https://rpc-mumbai.maticvigil.com",
-//       accounts: [process.env.PRIVATE_KEY]
-//     }
-//   },
-//   solidity: {
-//     version: "0.7.0",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-// }
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
+module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.7.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
