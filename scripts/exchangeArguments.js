828c0c9a51f90f9d4ae2126ec632d7f5bd61e4ce
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-04-04 16:30:58 -0500

    modifies exchangeArgs for deployment verification

--- scripts/exchangeArguments.js
+++ scripts/exchangeArguments.js
@@ -1,10 +1,10 @@
 module.exports = [
     // Deployed factory address
-    "0x7F1406d550802036ccd6f7CcdB40A73D893b5f07",
+    "0x221fec5340c8606728E2470Aeeb368e10BD96278",
     // Factory owner (deployer address)
     "0x1df13A64355BE17118a05C00dA9815DAa4ab1ea0",
     // Deployed OpenLabNFT address
-    "0xd6E806ebC6108851eFdd7151310fbC12d35417D3",
+    "0x3Cf622fd0d47E30cace6D9246CB69fefcFdfA8A0",
     // Deployed royalty percentage
     5,
 ];
