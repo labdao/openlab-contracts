7685ff02a84b1223915a3da992c9ef815355e6a1
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-05-02 01:23:59 -0500

    adds foundry

---
+++ test/ExchangeFactory.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "ds-test/test.sol";
+import "../src/ExchangeFactory.sol";
+
+contract ExchangeFactoryTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
